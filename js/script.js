2fe518c7e5c213f000ab5128890057a39953f335
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-07-29 15:26:16 +0200

    app js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
 $(document).ready(function(){
-    $('#messaggio').keydown(aggiungi);
+    $('#messaggio').keydown(aggiungi); // alla pressione del tasto invio 
     
     
-    if(orario < 10){
+    if(orario < 10){ // controllo sull'orario in minuti
         orario = "0" + orario;
         $('#mostra-orario').append(orario);
         $('#mostra-orario-random').append(orario);
     }
 
+    // mostra orario
     $('#mostra-orario').append(orario);
     $('#mostra-orario-random').append(orario);
     
-    
-        function aggiungi(event){
+    // funzione aggiungi
+    function aggiungi(event){
         if(event.which == 13 || event.keyCode == 13){
 
-            if($('#messaggio').val() !== ""){
+            if($('#messaggio').val() !== ""){ // controllo sul valore passante vuoto
                 
+                // prelevo il valore, lo assegno e lo vado a stampare 
                 var valore = $('#messaggio').val();
                 var elemento = $('#mostra').clone().append(valore).addClass("mostra").removeClass('nascondi');
                 $('.app-item-child-center-right').append(elemento);
                 
-
+                // funzione a tempo determinato
                 setTimeout(function(){
-                    var listaRandom=[
+                    var listaRandom=[ // array elementi
                         'ciao',
                         'come stai?',
                         'tutto okay',
@@ -31,15 +33,16 @@ $(document).ready(function(){
                         'allora ci vediamo in giro'
                     ];
 
-                    var indice=numeriRandom(0,(listaRandom.length-1));
+                    var indice=numeriRandom(0,(listaRandom.length-1)); // scelta randomica degli elementi dalla array 
+                    //lo assegno e lo vado a stampare 
                     var elementoRandom= $('#mostraRandom').clone().text(listaRandom[indice]).addClass("mostra").removeClass("nascondi");
                     $('.app-item-child-center-right').append(elementoRandom);
                     
                 },1000)
 
-                }
             }
         }
+    }
      
 });
 
