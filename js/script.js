cf8fc65718ac4074a1634235c5253469e681159a
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-07-30 16:43:46 +0200

    app js

--- js/script.js
+++ js/script.js
@@ -1,55 +1,60 @@
-$(document).ready(function(){
+ $(document).ready(function(){
     $('#messaggio').keydown(aggiungi); // alla pressione del tasto invio 
     
     
-    if(orario < 10){ // controllo sull'orario in minuti
-        orario = "0" + orario;
-        $('#mostra-orario').append(orario);
-        $('#mostra-orario-random').append(orario);
-    }else{
-        // mostra orario
-        $('#mostra-orario').append(orario);
-        $('#mostra-orario-random').append(orario);  //-> Non funziona! da rivedere
-    }
+   /*  if(orario < 10){ // controllo sull'orario in minuti
+       orario = "0" + orario;
+         $('#mostra-orario').append(orario);
+         $('#mostra-orario-random').append(orario);
+     }else{
+         // mostra orario
+         $('#mostra-orario').append(orario);
+         $('#mostra-orario-random').append(orario);  //-> Non funziona! da rivedere
+     } */
 
 
-    // funzione aggiungi
-    function aggiungi(event){
-        if(event.which == 13 || event.keyCode == 13){
+     $('#mostra-orario').append(orario);
 
-            if($('#messaggio').val() !== ""){ // controllo sul valore passante vuoto
+     // funzione aggiungi
+     function aggiungi(event){
+         if(event.which == 13 || event.keyCode == 13){
+
+             if($('#messaggio').val() !== ""){ // controllo sul valore passante vuoto
                 
-                // prelevo il valore, lo assegno e lo vado a stampare 
-                var valore = $('#messaggio').val();
-                var elemento = $('#mostra').clone().append(valore).addClass("mostra").removeClass('nascondi');
-                $('.app-item-child-center-right').append(elemento);
+                 // prelevo il valore, lo assegno e lo vado a stampare 
+                 var valore = $('#messaggio').val();
+                 var elemento = $('#mostra').clone().append(valore).addClass("mostra").removeClass('nascondi');
+                 $('.app-item-child-center-right').append(elemento);
                 
-                // funzione a tempo determinato
-                setTimeout(function(){
-                    var listaRandom=[ // array elementi
-                        'ciao',
-                        'come stai?',
-                        'tutto okay',
-                        'meglio così',
+                 // funzione a tempo determinato
+                 setTimeout(function(){
+                     var listaRandom=[ // array elementi
+                         'ciao',
+                         'come stai?',
+                         'tutto okay',
+                         'meglio così',
                         'allora ci vediamo in giro'
-                    ];
+                     ];
 
-                    var indice=numeriRandom(0,(listaRandom.length-1)); // scelta randomica degli elementi dalla array 
-                    //lo assegno e lo vado a stampare 
-                    var elementoRandom= $('#mostraRandom').clone().text(listaRandom[indice]).addClass("mostra").removeClass("nascondi");
-                    //$('#mostra-orario-random').append(orario);
-                    $('.app-item-child-center-right').append(elementoRandom);
+                     var indice=numeriRandom(0,(listaRandom.length-1)); // scelta randomica degli elementi dalla array 
+
+                     var elementoRandom= $('#mostraRandom').clone().text(listaRandom[indice]).addClass("mostra").removeClass("nascondi");//lo assegno e lo vado a stampare 
+                     $('#mostra-orario-random').append(orario);
+                     $('.app-item-child-center-right').append(elementoRandom);
+                     /* $('#mostraRandom').find('#mostra-orario-random').append(orario) */
+                     //$('#mostra-orario-random').append(orario);
                     
-                },1000)
+                    
+                 },1000)
 
-                $('#messaggio').remove(valore); //-> Non funziona! da rivedere
-                //console.log(valore)
+                 $('#messaggio').remove(valore); //-> Non funziona! da rivedere
+                 //console.log(valore)
                
-            }
+             }
             
             
-        }
-    }
+         }
+     }
      
 });
 
@@ -65,7 +70,19 @@ function numeriRandom(min,max){
 var tempo= new Date();
 var orario=tempo.getHours() + ":" + tempo.getMinutes();
 
+/* $(document).ready(function(){
+    $('#messaggio').keydown(function(e){ 
+        if(e.which == 13 || e.keyCode == 13){
+            invioMessaggio()
+        }
+    });
 
+    function invioMessaggio(){
+        var valoreInput=$('#messaggio').val();
+        var elementoClone = $('.template .mostra-messaggio').clone().addClass("inviato").find(".paragrafo").append(valoreInput);
+        $('.app-item-child-center-right .mostra').append(elementoClone);
+    }
+}); */
 
 
 
---
+++ js/script2.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+
+
+$("#messaggio").keydown(function(event){
+    if(event.which == 13 || event.keyCode == 13){
+        if($('#messaggio').val() !== ""){
+            invioMessaggio();
+            $('#messaggio').val('')
+        }
+    }
+})
+
+
+function invioMessaggio(){
+    var valore = $("#messaggio").val();
+    var clone = $(".template .message").clone();
+    clone.addClass("inviato");
+    clone.find(".paragrafo").append(valore);
+    clone.find(".orario").append(orario);
+    $(".app-item-child-center-right.active").append(clone);
+
+    //$('#messaggio').val('');
+    setTimeout(rispostaAutomatica,2000);
+}
+
+function rispostaAutomatica(){
+    var listaRandom=[
+        'ciao',
+        'come stai?',
+        'tutto okay',
+        'meglio così',
+       'allora ci vediamo in giro'
+    ];
+    var indice=numeriRandom(0,(listaRandom.length-1));
+
+    var clone2 = $(".template .message").clone();
+    clone2.addClass("ricevuto");
+    clone2.find(".paragrafo").append(listaRandom[indice])
+    clone2.find(".orario").append(orario);
+    $(".app-item-child-center-right.active").append(clone2);
+}
+
+})
+
+
+// funzione random
+function numeriRandom(min,max){
+    return Math.floor(Math.random()*(max - min + 1) + min);
+}
+
+// tempo
+var tempo= new Date();
+var orario=tempo.getHours() + ":" + tempo.getMinutes();
+
+
+// index e eq
+
+
+/* aggiungere una classe active, quando inviate un messaggio appenderlo alla classe active
+
+il messaggio che inviate lo deve appendere alla chat active
+anche nelle li mettere active
+*/
+
+$('.contact-element').click(function(){
+    var elementoAttivo=$('.contact-element.active-utente'); // seleziono l'utente
+    elementoAttivo.removeClass("active-utente");
+    var schermataAttiva=$('.app-item-child-center-right.active'); // seleziono la schermata attive
+    schermataAttiva.removeClass('active'); // rimuovo la classe a schermata attiva
+    $(this).addClass('active-utente'); // NON funziona provo ad aggiungere la classe alla prossima schermata attiva
+    var click=$(this).index(); // scopro l'indice dell'utente selezionato
+    var elemento=$('.app-item-child-center-right').eq(click).addClass('active'); 
+})
+
+/* index() è un metodo integrato in jQuery che viene utilizzato per restituire 
+l'indice degli elementi specificati rispetto al selettore. 
+
+$ (Selector) .index (elemento)
+
+eq() Il metodo eq () è un metodo integrato in jQuery che viene utilizzato
+per individuare direttamente 
+gli elementi selezionati e restituisce un elemento con indice specifico.
+
+$ (Selector) .eq (indice)
+*/
